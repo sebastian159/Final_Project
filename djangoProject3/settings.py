863e5ecc6a11191816e54ebf878abe60348e2625
sebastian mesa <msebastian2015@gmail.com>
2020-12-09 12:48:23 -0500

    added new pythonanywhere line

--- djangoProject3/settings.py
+++ djangoProject3/settings.py
@@ -124,5 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ['static']
-#STATIC_ROOT = BASE_DIR / 'static'
+#STATICFILES_DIRS = ['static']
+STATIC_ROOT = BASE_DIR / 'static'
